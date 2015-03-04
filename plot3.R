5fc466d8ba5e8c8ec237be35ebd58c0b271298b7
ouleye <ouleye.gueye@yahoo.fr>
2015-03-04 15:13:18 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -2,12 +2,13 @@ Sys.setlocale("LC_TIME", "English")
 
 ###WARNINGS##
 ## The file household_power_consumption.txt must be in the working directory (unzipped)##
-if(!file.exists("./household_power_consumption.txt")) { print("'household_power_consumption.txt' is not in the working directory"); break}
+if(!file.exists("./household_power_consumption.txt")) 
+                   { print("'household_power_consumption.txt' is not in the working directory"); break}
 
 # Load the data for 01/02/2007 and 02/02/2007.
 DATA <- read.table(text = grep("^[1,2]/2/2007", readLines("household_power_consumption.txt"), value = TRUE),
-                   col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity",
-                                 "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
+        col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity",
+        "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
 
 #Concatenate the time and Date variable, change it to a date format.
 DATA$Date <- strptime(paste(DATA$Date,DATA$Time), format='%d/%m/%Y %H:%M:%S')
@@ -22,5 +23,6 @@ with(DATA,plot(Date,Sub_metering_1, ylab = 'Energy sub metering',type='n'))
 with(DATA,points(Date,Sub_metering_1, col="black", type="l"))
 with(DATA,points(Date,Sub_metering_2, col="red", type="l"))
 with(DATA,points(Date,Sub_metering_3, col="blue", type="l"))
-legend("topright", col = c('black','red','blue'), legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),lty=c(1,1) )
+legend("topright", col = c('black','red','blue'), legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),
+                   lty=c(1,1) )
 dev.off()
