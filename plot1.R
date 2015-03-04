dba1009702fb2e4ccb3764e9c35a40b9c9ddf6ee
ouleye <ouleye.gueye@yahoo.fr>
2015-03-04 15:15:32 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -21,3 +21,5 @@ DATA$Time <- NULL
 png("plot1.png",width = 480, height = 480)
 with(DATA,hist(Global_active_power,col="red", xlab = 'Global Active Power (kilowatts)', main='Global Active Power'))
 dev.off()
+
+
